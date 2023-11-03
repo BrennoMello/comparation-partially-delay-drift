d66cad3561f25a3059ea38b7edac8a3bd61ffae7
Brenno Mello <brennodemello.bm@gmail.com>
2023-11-03 15:29:26 -0300

    update experiments v2

--- run_partially_experiments.py
+++ run_partially_experiments.py
@@ -43,8 +43,8 @@ def run_abrupt_agraw1(repetitions, learning_algorithms, drift_detectors_params, 
                                         -p {size["drift_position"][0]} -w 1) -e (BasicPartiallyLabeledConceptDriftPerformanceEvaluator) -i {size["data_size"]} -f 1000 -p {data_partially} -z {iter}" > results/abrupt_agraw1/{learning_algorithm}_{drift_algorithm["id"]}_{size["data_size"]}_{data_partially}_{iter}_AGRAW1_Abrupt.csv'
                                              
                     
-                        log.info(command)
-                        os.system(command)
+                            log.info(command)
+                            os.system(command)
 
 def run_gradual_agraw1(repetitions, learning_algorithms, drift_detectors_params, data_stream):
 
@@ -87,8 +87,8 @@ def run_gradual_agraw1(repetitions, learning_algorithms, drift_detectors_params,
                                         -p {size["drift_position"][1]} -w 500) \
                                         -p {size["drift_position"][0]} -w 500) -e (BasicPartiallyLabeledConceptDriftPerformanceEvaluator) -i {size["data_size"]} -f 1000 -p {data_partially} -z {iter}" > results/gradual_agraw1/{learning_algorithm}_{drift_algorithm["id"]}_{size["data_size"]}_{data_partially}_{iter}_AGRAW1_Gradual.csv'           
                         
-                        log.info(command)
-                        os.system(command)
+                            log.info(command)
+                            os.system(command)
 
 
 def run_abrupt_agraw2(repetitions, learning_algorithms, drift_detectors_params, data_stream):
@@ -132,8 +132,8 @@ def run_abrupt_agraw2(repetitions, learning_algorithms, drift_detectors_params, 
                                         -p {size["drift_position"][1]} -w 1) \
                                         -p {size["drift_position"][0]} -w 1) -e (BasicPartiallyLabeledConceptDriftPerformanceEvaluator) -i {size["data_size"]} -f 1000 -p {data_partially} -z {iter}" > results/abrupt_agraw2/{learning_algorithm}_{drift_algorithm["id"]}_{size["data_size"]}_{data_partially}_{iter}_AGRAW2_Abrupt.csv'
                                 
-                    log.info(command)
-                    os.system(command)
+                        log.info(command)
+                        os.system(command)
 
 def run_gradual_agraw2(repetitions, learning_algorithms, drift_detectors_params, data_stream):
 
@@ -175,8 +175,8 @@ def run_gradual_agraw2(repetitions, learning_algorithms, drift_detectors_params,
                                         -p {size["drift_position"][1]} -w 500) \
                                         -p {size["drift_position"][0]} -w 500) -e (BasicPartiallyLabeledConceptDriftPerformanceEvaluator) -i {size["data_size"]} -f 1000 -p {data_partially} -z {iter}" > results/gradual_agraw2/{learning_algorithm}_{drift_algorithm["id"]}_{size["data_size"]}_{data_partially}_{iter}_AGRAW2_Gradual.csv'
                                 
-                    log.info(command)
-                    os.system(command)
+                        log.info(command)
+                        os.system(command)
 
 def run_abrupt_led(repetitions, learning_algorithms, drift_detectors_params, data_stream):
 
@@ -218,8 +218,8 @@ def run_abrupt_led(repetitions, learning_algorithms, drift_detectors_params, dat
                                         -p {size["drift_position"][1]} -w 1) \
                                         -p {size["drift_position"][0]} -w 1) -e (BasicPartiallyLabeledConceptDriftPerformanceEvaluator) -i {size["data_size"]} -f 1000 -p {data_partially} -z {iter}" > results/abrupt_led/{learning_algorithm}_{drift_algorithm["id"]}_{size["data_size"]}_{data_partially}_{iter}_LED_Abrupt.csv'
                                 
-                    log.info(command)
-                    os.system(command)
+                        log.info(command)
+                        os.system(command)
 
 def run_gradual_led(repetitions, learning_algorithms, drift_detectors_params, data_stream):
 
@@ -261,8 +261,8 @@ def run_gradual_led(repetitions, learning_algorithms, drift_detectors_params, da
                                         -p {size["drift_position"][1]} -w 500) \
                                         -p {size["drift_position"][0]} -w 500) -e (BasicPartiallyLabeledConceptDriftPerformanceEvaluator) -i {size["data_size"]} -f 1000 -p {data_partially} -z {iter}" > results/gradual_led/{learning_algorithm}_{drift_algorithm["id"]}_{size["data_size"]}_{data_partially}_{iter}_LED_Gradual.csv'
                                 
-                    log.info(command)
-                    os.system(command)
+                        log.info(command)
+                        os.system(command)
 
 
 def run_abrupt_mixed(repetitions, learning_algorithms, drift_detectors_params, data_stream):
@@ -306,8 +306,8 @@ def run_abrupt_mixed(repetitions, learning_algorithms, drift_detectors_params, d
                                         -p {size["drift_position"][1]} -w 1) \
                                         -p {size["drift_position"][0]} -w 1) -e (BasicPartiallyLabeledConceptDriftPerformanceEvaluator) -i {size["data_size"]} -f 1000 -p {data_partially} -z {iter}" > results/abrupt_mixed/{learning_algorithm}_{drift_algorithm["id"]}_{size["data_size"]}_{data_partially}_{iter}_MIXED_Abrupt.csv'                                
                     
-                    log.info(command)
-                    os.system(command)
+                        log.info(command)
+                        os.system(command)
 
 
 def run_gradual_mixed(repetitions, learning_algorithms, drift_detectors_params, data_stream):
@@ -351,8 +351,8 @@ def run_gradual_mixed(repetitions, learning_algorithms, drift_detectors_params, 
                                         -p {size["drift_position"][1]} -w 500) \
                                         -p {size["drift_position"][0]} -w 500) -e (BasicPartiallyLabeledConceptDriftPerformanceEvaluator) -i {size["data_size"]} -f 1000 -p {data_partially} -z {iter}" > results/gradual_mixed/{learning_algorithm}_{drift_algorithm["id"]}_{size["data_size"]}_{data_partially}_{iter}_MIXED_Gradual.csv'            
                         
-                    log.info(command)
-                    os.system(command)
+                        log.info(command)
+                        os.system(command)
 
 def run_abrupt_randomRBF(repetitions, learning_algorithms, drift_detectors_params, data_stream):
 
@@ -395,8 +395,8 @@ def run_abrupt_randomRBF(repetitions, learning_algorithms, drift_detectors_param
                                         -p {size["drift_position"][1]} -w 1) \
                                         -p {size["drift_position"][0]} -w 1) -e (BasicPartiallyLabeledConceptDriftPerformanceEvaluator) -i {size["data_size"]} -f 1000 -p {data_partially} -z {iter}" > results/abrupt_randomRBF/{learning_algorithm}_{drift_algorithm["id"]}_{size["data_size"]}_{data_partially}_{iter}_RANDOM_RBF_Abrupt.csv'       
                         
-                    log.info(command)
-                    os.system(command)
+                        log.info(command)
+                        os.system(command)
 
 def run_gradual_randomRBF(repetitions, learning_algorithms, drift_detectors_params, data_stream):
 
@@ -438,8 +438,8 @@ def run_gradual_randomRBF(repetitions, learning_algorithms, drift_detectors_para
                                         -p {size["drift_position"][1]} -w 500) \
                                         -p {size["drift_position"][0]} -w 500) -e (BasicPartiallyLabeledConceptDriftPerformanceEvaluator) -i {size["data_size"]} -f 1000 -p {data_partially} -z {iter}" > results/gradual_randomRBF/{learning_algorithm}_{drift_algorithm["id"]}_{size["data_size"]}_{data_partially}_{iter}_RANDOM_RBF_Gradual.csv'
                                                             
-                    log.info(command)
-                    os.system(command)
+                        log.info(command)
+                        os.system(command)
 
 
 def run_abrupt_sine(repetitions, learning_algorithms, drift_detectors_params, data_stream):
@@ -482,8 +482,8 @@ def run_abrupt_sine(repetitions, learning_algorithms, drift_detectors_params, da
                                         -p {size["drift_position"][1]} -w 1) \
                                         -p {size["drift_position"][0]} -w 1) -e (BasicPartiallyLabeledConceptDriftPerformanceEvaluator) -i {size["data_size"]} -f 1000 -p {data_partially} -z {iter}" > results/abrupt_sine/{learning_algorithm}_{drift_algorithm["id"]}_{size["data_size"]}_{data_partially}_{iter}_SINE_Abrupt.csv'        
                         
-                    log.info(command)
-                    os.system(command)
+                        log.info(command)
+                        os.system(command)
 
 
 def run_gradual_sine(repetitions, learning_algorithms, drift_detectors_params, data_stream):
@@ -527,8 +527,8 @@ def run_gradual_sine(repetitions, learning_algorithms, drift_detectors_params, d
                                         -p {size["drift_position"][1]} -w 500) \
                                         -p {size["drift_position"][0]} -w 500) -e (BasicPartiallyLabeledConceptDriftPerformanceEvaluator) -i {size["data_size"]} -f 1000 -p {data_partially} -z {iter}" > results/gradual_sine/{learning_algorithm}_{drift_algorithm["id"]}_{size["data_size"]}_{data_partially}_{iter}_SINE_Gradual.csv'
                         
-                    log.info(command)
-                    os.system(command)
+                        log.info(command)
+                        os.system(command)
 
 def run_abrupt_waveform(repetitions, learning_algorithms, drift_detectors_params, data_stream):
 
@@ -571,8 +571,8 @@ def run_abrupt_waveform(repetitions, learning_algorithms, drift_detectors_params
                                         -p {size["drift_position"][1]} -w 1) \
                                         -p {size["drift_position"][0]} -w 1) -e (BasicPartiallyLabeledConceptDriftPerformanceEvaluator) -i {size["data_size"]} -f 1000 -p {data_partially} -z {iter}" > results/abrupt_waveform/{learning_algorithm}_{drift_algorithm["id"]}_{size["data_size"]}_{data_partially}_{iter}_WAVEFORM_Abrupt.csv'                
                         
-                    log.info(command)
-                    os.system(command)
+                        log.info(command)
+                        os.system(command)
 
 
 def run_gradual_waveform(repetitions, learning_algorithms, drift_detectors_params, data_stream):
@@ -616,8 +616,8 @@ def run_gradual_waveform(repetitions, learning_algorithms, drift_detectors_param
                                         -p {size["drift_position"][1]} -w 500) \
                                         -p {size["drift_position"][0]} -w 500) -e (BasicPartiallyLabeledConceptDriftPerformanceEvaluator) -i {size["data_size"]} -f 1000 -p {data_partially} -z {iter}" > results/gradual_waveform/{learning_algorithm}_{drift_algorithm["id"]}_{size["data_size"]}_{data_partially}_{iter}_WAVEFORM_Gradual.csv'
                         
-                    log.info(command)
-                    os.system(command)
+                        log.info(command)
+                        os.system(command)
 
 
 
@@ -663,33 +663,33 @@ def run_experiment(repetitions):
                             ]
     
                                                 
-    log.info(f'Runnig abrupt_agraw1')
-    run_abrupt_agraw1(repetitions, learning_algorithms, drift_detectors_params, data_stream)
-    log.info(f'Runnig gradual_agraw1')
-    run_gradual_agraw1(repetitions, learning_algorithms, drift_detectors_params, data_stream)
-    log.info(f'Runnig abrupt_agraw2')
-    run_abrupt_agraw2(repetitions, learning_algorithms, drift_detectors_params, data_stream)
-    log.info(f'Runnig gradual_agraw2')
-    run_gradual_agraw2(repetitions, learning_algorithms, drift_detectors_params, data_stream)
-    log.info(f'Runnig abrupt_led')
-    run_abrupt_led(repetitions, learning_algorithms, drift_detectors_params, data_stream)
-    log.info(f'Runnig gradual_led')
-    run_gradual_led(repetitions, learning_algorithms, drift_detectors_params, data_stream)
-    log.info(f'Runnig abrupt_mixed')
-    run_abrupt_mixed(repetitions, learning_algorithms, drift_detectors_params, data_stream)
-    log.info(f'Runnig gradual_mixed')
-    run_gradual_mixed(repetitions, learning_algorithms, drift_detectors_params, data_stream)
-    log.info(f'Runnig abrupt_randomRBF')
-    run_abrupt_randomRBF(repetitions, learning_algorithms, drift_detectors_params, data_stream)
-    log.info(f'Runnig gradual_randomRBF')
-    run_gradual_randomRBF(repetitions, learning_algorithms, drift_detectors_params, data_stream)
-    log.info(f'Runnig abrupt_sine')
-    run_abrupt_sine(repetitions, learning_algorithms, drift_detectors_params, data_stream)
-    log.info(f'Runnig gradual_sine')
-    run_gradual_sine(repetitions, learning_algorithms, drift_detectors_params, data_stream)
-    log.info(f'Runnig abrupt_waveform')
-    run_abrupt_waveform(repetitions, learning_algorithms, drift_detectors_params, data_stream)
-    log.info(f'Runnig gradual_waveform')
+    # log.info(f'Runnig abrupt_agraw1')
+    # run_abrupt_agraw1(repetitions, learning_algorithms, drift_detectors_params, data_stream)
+    # log.info(f'Runnig gradual_agraw1')
+    # run_gradual_agraw1(repetitions, learning_algorithms, drift_detectors_params, data_stream)
+    # log.info(f'Runnig abrupt_agraw2')
+    # run_abrupt_agraw2(repetitions, learning_algorithms, drift_detectors_params, data_stream)
+    # log.info(f'Runnig gradual_agraw2')
+    # run_gradual_agraw2(repetitions, learning_algorithms, drift_detectors_params, data_stream)
+    # log.info(f'Runnig abrupt_led')
+    # run_abrupt_led(repetitions, learning_algorithms, drift_detectors_params, data_stream)
+    # log.info(f'Runnig gradual_led')
+    # run_gradual_led(repetitions, learning_algorithms, drift_detectors_params, data_stream)
+    # log.info(f'Runnig abrupt_mixed')
+    # run_abrupt_mixed(repetitions, learning_algorithms, drift_detectors_params, data_stream)
+    # log.info(f'Runnig gradual_mixed')
+    # run_gradual_mixed(repetitions, learning_algorithms, drift_detectors_params, data_stream)
+    # log.info(f'Runnig abrupt_randomRBF')
+    # run_abrupt_randomRBF(repetitions, learning_algorithms, drift_detectors_params, data_stream)
+    # log.info(f'Runnig gradual_randomRBF')
+    # run_gradual_randomRBF(repetitions, learning_algorithms, drift_detectors_params, data_stream)
+    # log.info(f'Runnig abrupt_sine')
+    # run_abrupt_sine(repetitions, learning_algorithms, drift_detectors_params, data_stream)
+    # log.info(f'Runnig gradual_sine')
+    # run_gradual_sine(repetitions, learning_algorithms, drift_detectors_params, data_stream)
+    # log.info(f'Runnig abrupt_waveform')
+    # run_abrupt_waveform(repetitions, learning_algorithms, drift_detectors_params, data_stream)
+    # log.info(f'Runnig gradual_waveform')
     run_gradual_waveform(repetitions, learning_algorithms, drift_detectors_params, data_stream)
     #log.info(f'Runnig abrupt_hyperplane')        
 
